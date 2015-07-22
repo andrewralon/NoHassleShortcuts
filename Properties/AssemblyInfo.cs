9fa6e524c57a9dba61bf900d3bf397d1de24264e
Andrew Ralon <acralon@gmail.com>
2015-07-21 22:31:15 -0400

    Trying out "TeamRalon" instead of "Team Ralon"....

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NoHassleShortcuts")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Team Ralon")]
+[assembly: AssemblyCompany("TeamRalon")]
 [assembly: AssemblyProduct("NoHassleShortcuts")]
-[assembly: AssemblyCopyright("Copyright © Team Ralon 2015")]
+[assembly: AssemblyCopyright("Copyright © TeamRalon 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1.2")]
+[assembly: AssemblyVersion("0.1.1.3")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
